2c2d6515fbed4ce617b69814f0606b19bb6e239b
Jamie <james@areafamily.us>
2022-03-15 18:51:34 -0500

    Basic Express server up and running!

---
+++ README.md
@@ -0,0 +1,12 @@
+## March 15th, 2022 ##
+
+Setting up the Books API Project Checklist:
+- Create MVC folders
+- Create .env file
+- Create .gitignore file
+- Create index.js
+- Setup Github rep and do initial commit
+- Run:
+    - 'npm init -y' to create package.json file with defaults
+    - 'npm install express' to make our project an Express framework application
+    - 'npm i dotenv' so our project uses the .env file
--- index.js
+++ index.js
@@ -1,14 +1,29 @@
+/////////////////////
 //Modules and Globals
-require('dotenv').config()
-const express = require('express')
-const app = express()
+/////////////////////
+require('dotenv').config() // Initiate .env file for environment variables
+const express = require('express') // Instantiate express
+const app = express() // Assign express constructor to a variable
 
+///////////////////
+// Express Settings
+///////////////////
+
+// Configure Express body-parser for JSON
+app.use(express.json())
+
+//////////////////////////////////
+// Controllers and Routes Settings
+//////////////////////////////////
+
+// GET Endpoint for '/' or homepage
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+    res.send('Welcome to the Books API!')
 })
 
+// GET Wildcard route - MUST be at end of the list of endpoints so it doesn't override other endpoints
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
+    res.status(404).send('<h1>404 - Sorry, that page has been checked out from the library!</h1>')
 })
-
-app.listen(process.env.PORT)
+// Keep server socket open with listening to a PORT from the .env file and log to console "I'm awake" so we know the server is open
+app.listen(process.env.PORT, console.log("Reading Books on Port 3000!"))
