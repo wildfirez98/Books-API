e3b21c950c0a99303a8c50ec3891b3514c0a2424
Jamie <james@areafamily.us>
2022-03-17 11:20:53 -0500

    Seed route update

--- controllers/books.js
+++ controllers/books.js
@@ -1,43 +1,43 @@
 const books = require('express').Router() // Assign our Express router to a variable so we can call it 
 const Book = require('../models/books.js')
 
-// Seed Data - Comment out after initial population
-//books.get('/seed', (req, res) => {
-//    Book.insertMany([{
-//        "title": "The Shinobi Initiative",
-//        "description": "The reality-bending adventures of a clandestine service agency in the year 2166",
-//        "year": 2014,
-//        "quantity": 10,
-//        "imageURL": "https://imgur.com/LEqsHy5.jpeg"
-//      },
-//      {
-//        "title": "Tess the Wonder Dog",
-//        "description": "The tale of a dog who gets super powers",
-//        "year": 2007,
-//        "quantity": 3,
-//        "imageURL": "https://imgur.com/cEJmGKV.jpg"
-//      },
-//      {
-//        "title": "The Annals of Arathrae",
-//        "description": "This anthology tells the intertwined narratives of six fairy tales.",
-//        "year": 2016,
-//        "quantity": 8,
-//        "imageURL": "https://imgur.com/VGyUtrr.jpeg"
-//      },
-//      {
-//        "title": "W∀RP",
-//        "description": "A time-space anomaly folds matter from different points in earth's history in on itself, sending six unlikely heroes on a race against time as worlds literally collide.",
-//        "year": 2010,
-//        "quantity": 4,
-//        "imageURL": "https://imgur.com/qYLKtPH.jpeg"
-//      }])
-//        .then(res.status(200).json({
-//            message: 'Seed successful'
-//        }))
-//        .catch(res.status(400).json({
-//            message: 'Seed unsuccessful'
-//        }))
-//})
+//Seed Data - Comment out after initial population
+books.get('/seed', (req, res) => {
+   Book.insertMany([{
+        "title": "The Shinobi Initiative",
+        "description": "The reality-bending adventures of a clandestine service agency in the year 2166",
+        "year": 2014,
+        "quantity": 10,
+        "imageURL": "https://imgur.com/LEqsHy5.jpeg"
+      },
+      {
+        "title": "Tess the Wonder Dog",
+        "description": "The tale of a dog who gets super powers",
+        "year": 2007,
+        "quantity": 3,
+        "imageURL": "https://imgur.com/cEJmGKV.jpg"
+      },
+      {
+        "title": "The Annals of Arathrae",
+        "description": "This anthology tells the intertwined narratives of six fairy tales.",
+        "year": 2016,
+        "quantity": 8,
+        "imageURL": "https://imgur.com/VGyUtrr.jpeg"
+      },
+      {
+        "title": "W∀RP",
+        "description": "A time-space anomaly folds matter from different points in earth's history in on itself, sending six unlikely heroes on a race against time as worlds literally collide.",
+        "year": 2010,
+        "quantity": 4,
+        "imageURL": "https://imgur.com/qYLKtPH.jpeg"
+      }])
+        .then(res.status(200).json({
+            message: 'Seed successful'
+        }))
+        .catch(res.status(400).json({
+            message: 'Seed unsuccessful'
+        }))
+})
 
 // GET - Index Route
 books.get('/', (req, res, next) => {
